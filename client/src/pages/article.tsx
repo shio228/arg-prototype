cc5ee5a89391c9e2c0f42c7910887033d8643dbd
shio228 <yutaka.p2@gmail.com>
2026-05-07 14:44:49 +0900

    feat: サムネイルのフルサイズ画像を表示

--- client/src/pages/article.tsx
+++ client/src/pages/article.tsx
@@ -36,14 +36,32 @@ export default function Article() {
     });
   }, [id]);
 
+  const [imageOpen, setImageOpen] = useState(false);
+
   if (!article) return <div>Loading...</div>;
 
   return (
     <div className="max-w-5xl mx-auto p-4">
-      <img
-        src={article.thumbnail}
-        className="w-full h-48 object-cover mb-4"
-      />
+      {article.thumbnail && (
+        <img
+          src={article.thumbnail}
+          className="w-full h-48 object-cover mb-4 cursor-pointer"
+          onClick={() => setImageOpen(true)}
+        />
+      )}
+
+      {imageOpen && (
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black/70"
+          onClick={() => setImageOpen(false)}
+        >
+          <img
+            src={article.thumbnail}
+            className="max-w-[90vw] max-h-[90vh] object-contain"
+            onClick={(e) => e.stopPropagation()}
+          />
+        </div>
+      )}
 
       <div className="flex gap-8">
         {/* 左：記事本文 */}
